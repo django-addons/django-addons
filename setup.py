35a34e06b3af0368794af3a1ba10b22304b17f43
Stefan Foulis <stefan.foulis@sherpany.com>
2018-03-20 23:51:31 +0100

    python 3.6 only

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     author_email='info@divio.ch',
     url='https://github.com/django-addons/django-addons',
     packages=find_packages(),
+    python_requires='>=3.6',
     install_requires=[
         # Django dependency is intentionally left out as a workaround for pips
         # ignorance regarding exact versions to install.

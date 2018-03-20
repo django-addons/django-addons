a80755c1f3832c55e90d2d652dc8d8d2c6ae219a
Stefan Foulis <stefan.foulis@sherpany.com>
2018-03-20 22:36:28 +0100

    WIP

--- django_addons/urls.py
+++ django_addons/urls.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings, urls
+from django.conf import settings
+from django.urls import include, path
 
 
 def patterns():
     pats = [
-        urls.url(r'^', urls.include(url))
+        path('', include(url))
         for url in getattr(settings, 'ADDON_URLS', [])
     ]
     last_url = getattr(settings, 'ADDON_URLS_LAST', None)
     if last_url:
         pats.append(
-            urls.url(r'^', urls.include(last_url))
+            path('', include(last_url))
         )
     return pats
 
 
 def i18n_patterns():
     pats = [
-        urls.url(r'^', urls.include(url))
+        path('', include(url))
         for url in getattr(settings, 'ADDON_URLS_I18N', [])
     ]
     last_url = getattr(settings, 'ADDON_URLS_I18N_LAST', None)
     if last_url:
         pats.append(
-            urls.url(r'^', urls.include(last_url))
+            path('', include(last_url))
         )
     return pats
